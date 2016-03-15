f1cb82dcfe4776be6eb4213af4dd1a619cf633a2
imchengr <imchengr@gmail.com>
2016-03-16 01:17:00 +0800

    加入場景二 第一集買了一本，第二集也買了一本，價格應為100*2*0.95=190 紅燈出現 [註]每一個場景都是獨立的,無法預先了解需求 當需求一個一個出來時,針對每一個場景修改unittest, 並增加production code,同時重構

--- PotterShoppingCart.Test/UnitTest1.cs
+++ PotterShoppingCart.Test/UnitTest1.cs
@@ -23,5 +23,23 @@ namespace PotterShoppingCart.Test
             //assert
             Assert.AreEqual(expect, actual);
         }
+
+        [TestMethod]
+        public void 第一集_一本_第二本_一本_總金額_190()
+        {
+            //arrange
+            List<BookOrder> BookList = new List<BookOrder>(){
+                new BookOrder() { BookName="HP1", Quantity=1 },
+                new BookOrder() { BookName="HP2", Quantity=1 }
+            };
+            Cashier cashier = new Cashier();
+            int expect = 190;
+
+            //act
+            int actual = cashier.check(BookList);
+
+            //assert
+            Assert.AreEqual(expect, actual);
+        }
     }
 }
