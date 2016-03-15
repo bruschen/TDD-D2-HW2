92a0415de89fc86d991b15ea527b6ab2defeeab2
imchengr <imchengr@gmail.com>
2016-03-16 01:59:28 +0800

    加入場景三 Scenario: 一二三集各買了一本，價格應為100*3*0.9=270 紅燈出現

--- PotterShoppingCart.Test/UnitTest1.cs
+++ PotterShoppingCart.Test/UnitTest1.cs
@@ -41,5 +41,24 @@ namespace PotterShoppingCart.Test
             //assert
             Assert.AreEqual(expect, actual);
         }
+
+        [TestMethod]
+        public void 第一集_一本_第二本_一本_第三本_一本_總金額_270()
+        {
+            //arrange
+            List<BookOrder> BookList = new List<BookOrder>(){
+                new BookOrder() { BookName="HP1", Quantity=1 },
+                new BookOrder() { BookName="HP2", Quantity=1 },
+                new BookOrder() { BookName="HP3", Quantity=1 },
+            };
+            Cashier cashier = new Cashier();
+            int expect = 270;
+
+            //act
+            int actual = cashier.check(BookList);
+
+            //assert
+            Assert.AreEqual(expect, actual);
+        }
     }
 }
